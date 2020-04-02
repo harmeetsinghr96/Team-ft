7331b813e4495c854e1273e4268d0ed5711fe542
harmeetsinghr96 <harmeetsinghr96@gmail.com>
2020-04-02 17:24:40 +0530

    added form to forgot password

--- src/app/modules/public/forgot-password/forgot-password.component.ts
+++ src/app/modules/public/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  public formData: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  forgot($ev, values) {
+    console.log(values);
+    $ev.preventDefault();
+
+    // tslint:disable-next-line: forin
+    for (const control in this.formData.controls) {
+      this.formData.controls[control].markAsTouched();
+    }
+  }
+
+  private initForm() {
+    const emailExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    this.formData = new FormGroup({
+      email: new FormControl(null, Validators.compose([Validators.required, Validators.pattern(emailExp)])),
+    });
+  }
 }
